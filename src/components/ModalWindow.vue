515783c6a60eb35e80a493555983798346b7f3df
sbkwgh <sbkwgh@gmail.com>
2017-01-02 20:59:21 +0000

    Add scafolding for component

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class='modal_window'>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ModalWindow',
+		props: ['showModal'],
+		data () {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang='scss' scoped>
+	.modal_window {
+
+	}
+</style>
