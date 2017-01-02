7d8d4471a3e34896b0490d416f4a19c135c0069f
sbkwgh <sbkwgh@gmail.com>
2017-01-02 21:10:43 +0000

    Basic structure and css for modal

--- src/components/ModalWindow.vue
+++ src/components/ModalWindow.vue
@@ -1,5 +1,14 @@
 <template>
-	<div class='modal_window'>
+	<div class='modal_window__overlay'>
+		<div class='modal_window'>
+			Placeholder content<br/>
+			Placeholder content<br/>
+			Placeholder content<br/>
+			Placeholder content<br/>
+			Placeholder content<br/>
+			Placeholder content<br/>
+			Placeholder content<br/>
+		</div>
 	</div>
 </template>
 
@@ -16,7 +25,19 @@
 </script>
 
 <style lang='scss' scoped>
+	.modal_window__overlay {
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
 	.modal_window {
-
+		width: 20rem;
+		background-color: #fff;
 	}
 </style>
