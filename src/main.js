93aa159342484047a238db60cfcbc4c46ce39795
sbkwgh <sbkwgh@gmail.com>
2017-01-03 21:26:24 +0000

    More scaffolding and strucure

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 import App from './App'
+import store from './store/index'
 import Index from './components/routes/Index'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = new VueRouter({
 	routes: [
@@ -16,6 +19,7 @@ const router = new VueRouter({
 new Vue({
 	el: '#app',
 	template: '<App/>',
+	store,
 	components: { App },
 	router
 })
---
+++ src/store/mutations.js
@@ -0,0 +1,12 @@
+import Vuex from 'vuex'
+
+import * as actions from './actions'
+import * as getters from './getters'
+
+export default new Vuex.Store({
+	actions,
+	getters,
+	modules: {
+
+	}
+})
