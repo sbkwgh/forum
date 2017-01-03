fa7efa2c595dfbc87c6c583c98273944742da0c1
sbkwgh <sbkwgh@gmail.com>
2017-01-03 17:44:40 +0000

    Inital commit for fancy-input component

---
+++ src/components/FancyInput.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class='fancy_input'>
+		<div class='fancy_input__placeholder'>{{placeholder}}</div>
+		<input type='text' v-bind:value='value' v-on:input='updateValue($event.target.value)'>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'FancyInput',
+		props: ['value', 'placeholder'],
+		data () {
+			active: false
+		},
+		methods: {
+			updateValue (val) {
+				this.$emit('input', val);
+			}
+		}
+	}
+</script>
+
+<style lang='scss' scoped>
+	
+</style>
