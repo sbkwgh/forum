b7286a4b3149c5542c9966086ce93947a5c27f50
sbkwgh <sbkwgh@gmail.com>
2017-02-11 18:35:43 +0000

    Use es6 style

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    hash: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return User;
-};
+module.exports = (sequelize, DataTypes) => {
+	let User = sequelize.define('User', {
+		username: DataTypes.STRING,
+		hash: DataTypes.STRING
+	}, {
+		classMethods: {
+			associate: (models) => {
+				// associations can be defined here
+			}
+		}
+	})
+
+	return User
+}
