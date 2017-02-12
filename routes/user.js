391da7007ceca6106cda7764291cbc27b545445e
sbkwgh <sbkwgh@gmail.com>
2017-02-12 11:22:46 +0000

    Fix typo

--- routes/user.js
+++ routes/user.js
@@ -15,9 +15,9 @@ router.post('/', async (req, res) => {
 			if(typeof req.body.username !== 'string') {
 				validationErrors.push(Errors.invalidParameterType('username', 'string'))
 			} if(req.body.username.length < 6) {
-				validationErrors.push(Errors.parmeterLengthTooSmall('username', 6))
+				validationErrors.push(Errors.parameterLengthTooSmall('username', 6))
 			} if(req.body.username.length > 50) {
-				validationErrors.push(Errors.parmeterLengthTooLarge('username', 50))
+				validationErrors.push(Errors.parameterLengthTooLarge('username', 50))
 			}
 		}
 
@@ -27,13 +27,13 @@ router.post('/', async (req, res) => {
 			if(typeof req.body.password !== 'string') {
 				validationErrors.push(Errors.invalidParameterType('password', 'string'))
 			} if(req.body.password.length < 6) {
-				validationErrors.push(Errors.parmeterLengthTooSmall('password', 6))
+				validationErrors.push(Errors.parameterLengthTooSmall('password', 6))
 			} if(req.body.password.length > 100) {
-				validationErrors.push(Errors.parmeterLengthTooSmall('password', 100))
+				validationErrors.push(Errors.parameterLengthTooLarge('password', 100))
 			}
 		}
 
-		if(errors.length) throw Errors.VALIDATION_ERROR
+		if(validationErrors.length) throw Errors.VALIDATION_ERROR
 
 		user = await User.create({
 			username: req.body.username,
@@ -42,13 +42,17 @@ router.post('/', async (req, res) => {
 
 		res.json(user.toJSON())
 	} catch (err) {
+		console.log(err)
 		if(err === Errors.VALIDATION_ERROR) {
 			res.status(400)
 			res.json({
 				errors: validationErrors
 			})
 		} else {
-			res.json(err)
+			res.status(500)
+			res.json({
+				errors: [Errors.unknown]
+			})
 		}
 	}
 })
