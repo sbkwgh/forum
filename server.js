accc914eb7ddd31eb7d5594be058cb6ce764fd93
sbkwgh <sbkwgh@gmail.com>
2017-02-13 20:45:01 +0000

    Add session middle where to server

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ let config = require('./config/server.js')
 //Middle-ware
 let bodyParser = require('body-parser')
 let morgan = require('morgan')
+let session = require('express-session')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+	secret: config.sessionSecret,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {}
+}))
 
 if(process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'production') {
 	app.use(morgan('dev'))
+}
+if(process.env.NODE_ENV === 'production') {
+	app.set('trust proxy', 1);
+	session.cookie.secure = true
 }
 
 app.use('/api/v1/user', require('./routes/user'))
