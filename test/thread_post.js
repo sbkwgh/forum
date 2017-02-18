a938f97baf1c734fd5821407d6298fb95553ea65
sbkwgh <sbkwgh@gmail.com>
2017-02-18 15:35:53 +0000

    Implement test skeleton

--- test/thread_post.js
+++ test/thread_post.js
@@ -14,6 +14,8 @@ chai.use(require('chai-things'))
 describe('Thread and post', () => {
 	//Wait for app to start before commencing
 	before((done) => {
+		if(server.locals.appStarted) done()
+
 		server.on('appStarted', () => {
 			done()
 		})
@@ -23,5 +25,35 @@ describe('Thread and post', () => {
 	//tests completed
 	after(() => {
 		sequelize.sync({ force: true })
+	})
+
+	describe('POST /thread', () => {
+		it('should create a thread if logged in', async () => {})
+		it('should return an error if not logged in', async () => {})
+		it('should return an error if missing parameters', async () => {})
+		it('should return an error if invalid types', async () => {})
+		it('should return an error if category does not exist', async () => {})
+	})
+
+	describe('GET /thread/:id', () => {
+		it('should return the thread and corresponding posts', async () => {})
+		it('should return an error if :id is invalid', async () => {})
+	})
+
+	describe('POST /post', () => {
+		it('should create a post if logged in', async () => {})
+		it('should return an error if not logged in', async () => {})
+		it('should return an error if missing parameters', async () => {})
+		it('should return an error if invalid types', async () => {})
+		it('should return an error if thread id does not exist', async () => {})
+
+		it('should be able to reply to a post', async () => {})
+		it('should return an error if reply id does not exist', async () => {})
+		it('should return an error if post reply not in same thread', async () => {})
+	})
+
+	describe('GET /post/:id', () => {
+		it('should return the post', async () => {})
+		it('should return an error if invalid post id', async () => {})
 	})
 })
