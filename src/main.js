41d8bcb3816a1613d8a16aa8b5012b20ddef6b88
sbkwgh <sbkwgh@gmail.com>
2017-02-22 15:26:45 +0000

    Add axios dependency

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App'
 import store from './store/index'
 
@@ -21,6 +24,7 @@ onResize('.index_categories', 'index_category');
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(VueAxios, axios)
 
 const router = new VueRouter({
 	routes: [
