0d1439e0ad8fcf610fca940fb85dedb0d66c11f0
sbkwgh <sbkwgh@gmail.com>
2017-03-25 12:26:03 +0000

    Add new component scaffolding

---
+++ src/components/ThreadDisplay.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class='thread_display'>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ThreadDisplay',
+		props: ['thread']
+	}
+</script>
+
+<style lang='scss' scoped>
+	.thread_display {
+		
+	}
+</style>
