1cfb38df8cd2e43ce5d8867386858ed19ea14676
sbkwgh <sbkwgh@gmail.com>
2017-04-04 22:08:16 +0100

    Add loading button

--- src/components/routes/SettingsGeneral.vue
+++ src/components/routes/SettingsGeneral.vue
@@ -7,23 +7,25 @@
 				Write something about yourself to be displayed on your user page
 			</p>
 			<fancy-textarea
-				placeholder='About me'
+				placeholder='About me description'
 				v-model='description.value'
 				:error='description.error'
 				type='password'
-			></fancy-input>
-			<button class='button button--green'>Save</button>
+			></fancy-textarea>
+			<loading-button class='button button--green'>Save description</loading-button>
 		</p>
 	</div>
 </template>
 
 <script>
 	import FancyTextarea from '../FancyTextarea'
+	import LoadingButton from '../LoadingButton'
 
 	export default {
 		name: 'settingsGeneral',
 		components: {
-			FancyTextarea
+			FancyTextarea,
+			LoadingButton
 		},
 		data () {
 			return {
