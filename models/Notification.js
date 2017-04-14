d4a22777fc06fc6e382a279d16dc4e0f6ae9122a
sbkwgh <sbkwgh@gmail.com>
2017-04-14 23:46:55 +0100

    Remove title field, change 'read' to 'interacted'

--- models/Notification.js
+++ models/Notification.js
@@ -1,12 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
 	let Notification = sequelize.define('Notification', {
-		title: DataTypes.TEXT,
-		read: DataTypes.BOOLEAN,
+		interacted: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false
+		},
 		type: DataTypes.ENUM('mention', 'thread update') 
 	}, {
 		classMethods: {
 			associate (models) {
-				Notification.hasOne(models.MentionNotification, { as: 'Data' })
+				Notification.hasOne(models.MentionNotification)
 				Notification.belongsTo(models.User)
 			}
 		}
