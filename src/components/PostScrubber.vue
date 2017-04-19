89aeabc2b09de7ba448984de0da9f2f0ff8846c5
sbkwgh <sbkwgh@gmail.com>
2017-04-19 10:53:51 +0100

    Add the line

--- src/components/PostScrubber.vue
+++ src/components/PostScrubber.vue
@@ -1,5 +1,7 @@
 <template>
 	<div class='post_scrubber'>
+		<div class='post_scrubber__line'></div>
+		<div class='post_scrubber__dragger'></div>
 	</div>
 </template>
 
@@ -14,6 +16,16 @@
 	@import '../assets/scss/variables.scss';
 
 	.post_scrubber {
-		
+		height: 10rem;
+		position: fixed;
+		right: calc(10% + 5rem);
+		margin-top: 4.5rem;
+
+		@at-root #{&}__line {
+			height: 100%;
+			background-color: $color__gray--darker;
+			border-radius: 1rem;
+			width: 0.125rem;
+		}
 	}
 </style>
