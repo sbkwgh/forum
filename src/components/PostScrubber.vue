488ff3a943059b0c5aaed36f5fa10641b68a5f64
sbkwgh <sbkwgh@gmail.com>
2017-04-19 10:45:48 +0100

    Add post-scrubber scaffolding

---
+++ src/components/PostScrubber.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class='post_scrubber'>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'PostScrubber'
+	}
+</script>
+
+
+<style lang='scss' scoped>
+	@import '../assets/scss/variables.scss';
+
+	.post_scrubber {
+		
+	}
+</style>
