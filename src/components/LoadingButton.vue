d53c4104decd5b6444919d289daa30e7faf3a668
sbkwgh <sbkwgh@gmail.com>
2017-05-22 08:56:32 +0100

    Use LoadingIcon component

--- src/components/LoadingButton.vue
+++ src/components/LoadingButton.vue
@@ -5,11 +5,7 @@
 		@click='event("click")'
 		@keydown='event("keydown")'
 	>
-		<div class='loading_button__icon' :class='{"loading_button__icon--dark": dark}'>
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
+		<loading-icon :dark='dark' class='loading_button__icon'></loading-icon>
 		<div class='loading_button__slot'>
 			<slot></slot>
 		</div>
@@ -17,9 +13,12 @@
 </template>
 
 <script>
+	import LoadingIcon from './LoadingIcon'
+
 	export default {
 		name: 'LoadingButton',
 		props: ['loading', 'dark'],
+		components: { LoadingIcon },
 		methods: {
 			event (name) {
 				if(this.loading) {
@@ -35,58 +34,22 @@
 <style lang='scss'>
 	@import '../assets/scss/variables.scss';
 
-	@keyframes loading {
-		0% {
-			transform: scale(0.75);
-		}
-		50% {
-			transform: scale(1.25);
-		}
-		100% {
-			transform: scale(0.75);
-		}
-	}
-
 	.loading_button {
 		position: relative;
-
-		@at-root #{&}__icon {
-			transition: all 0.2s;
-			position: absolute;
-			width: calc(100% - 1rem);
-			opacity: 0;
-			display: flex;
-			justify-content: center;
-			height: calc(100% - 0.85rem);
-			align-items: center;
-
-			pointer-events: none;
-
-			@at-root #{&}--dark {
-				span {
-					background-color: $color__darkgray--primary !important;
-				}
-			}
-
-			span {
-				height: 0.5rem;
-				width: 0.5rem;
-				display: inline-block;
-				border-radius: 100%;
-				background-color: rgba(256,256,256,0.9);
-				animation-name: loading;
-				animation-duration: 1s;
-				animation-timing-function: linear;
-				animation-iteration-count: infinite;
-				margin: 0 0.25rem;
-
-				&:nth-child(2n) { animation-delay: 0.333s; }
-				&:nth-child(3n) { animation-delay: 0.666s; }
-			}
-		}
+	
 		@at-root #{&}__slot {
 			transition: all 0.2s;
 			opacity: 1;
+		}
+
+
+		@at-root #{&}__icon {
+			position: absolute;
+			width: calc(100% - 1rem);
+			opacity: 0;
+			justify-content: center;
+			height: calc(100% - 0.85rem);
+			align-items: center;
 		}
 	}
 
