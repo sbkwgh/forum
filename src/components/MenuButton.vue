9b8f131f8157e4d224c9e07c72add9ae774bb3d7
sbkwgh <sbkwgh@gmail.com>
2017-05-25 21:11:44 +0100

    Bootstrap menu-button component

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class='menu_button'>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'MenuButton'
+	}
+</script>
+
+<style lang='scss' scoped>
+	@import '../assets/scss/variables.scss';
+
+	.menu_button {
+		
+	}
+</style>
