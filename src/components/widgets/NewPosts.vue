c6111f66dc10ce594b9c59ab11866c69d04227d6
sbkwgh <sbkwgh@gmail.com>
2017-06-14 18:19:42 +0100

    Add new widget component

---
+++ src/components/widgets/NewPosts.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class='widgets__new_post'></div>
+</template>
+
+<script>
+	export default {
+		name: 'NewPosts'
+	}
+</script>
+
+<style lang='scss' scoped>
+	.widgets__new_post {
+	}
+</style>
