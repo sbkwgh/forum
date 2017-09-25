b7e127bf0bb9dd62784d7ec8b457a90289824c14
sbkwgh <5166028+sbkwgh@users.noreply.github.com>
2017-09-25 21:10:11 +0100

    Add bans table migration

---
+++ migrations/20170925191833-create-bans-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('bans', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      canCreatePosts: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      canCreateThreads: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      ipBanned: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      message: Sequelize.TEXT,
+      UserId: Sequelize.INTEGER,
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('bans');
+  }
+};
